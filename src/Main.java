ddb204b598c569ee9255f943b6140f05391513ce
JDWdonga21 <ongame21212144@gmail.com>
2022-11-29 22:07:48 +0900

    println, 변수 실습

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        //프로그램의 시작 지점
+        System.out.println("Hello world!"); //""은 문자열(String)을 의미
+        System.out.println("문자열을 작성");
+        System.out.print("줄바꿈 \n");
+        System.out.println("자동으로 줄바꿈");
 
-        System.out.println("Hello world!");
+        System.out.print("만나서 반갑습니다.\n빨리 안드로이드 앱을 만들어보고 싶습니다.\n모두 화이팅합시다.\n\n");
+        //변수 (변할 수 있는 값 또는 수)
+        //자료형 String(문자열), int(정수), boolean(참 또는 거짓)
+        String a = "a 변수";
+        System.out.println(a);
+        a = "변하는 변수";
+        System.out.println(a);
+
+        int aInt = 10;
+        System.out.println(aInt);
+
+        boolean aBool = true;
+        System.out.println(aBool);
     }
 }
