e44297963f4d797af2fd4ed242d139ef326eca53
JDWdonga21 <ongame21212144@gmail.com>
2022-12-12 01:26:17 +0900

    숫자를 입력받고 숫자보다 작은 소수의 갯수를 리턴하는 코드 (인공지능 개발)

---
+++ src/PrimeNumber.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int inputNum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("소수의 갯수를 구할 숫자를 입력해주세요.");
+        inputNum = sc.nextInt();
+        PrimeNumber pn = new PrimeNumber();
+        System.out.print(inputNum + "의 소수의 갯수는 ");
+        System.out.println(pn.countPrimes(inputNum) + "개 입니다.");
+    }
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = 2; i * j < n; j++) {
+                    isPrime[i * j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        return count;
+    }
+}
+
